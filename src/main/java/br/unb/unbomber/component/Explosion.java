35a30ec5cb9f45938e5a0d3398fb56383fcf2e1f
grodrigues <Gabriel Rodrigues>
2014-09-23 13:13:08 -0300

    Melhorando descrição da arquitetura. Inserindo um teste na descrição do módulo de deslocamento.

---
+++ src/main/java/br/unb/unbomber/component/Explosion.java
@@ -0,0 +1,25 @@
+package br.unb.unbomber.component;
+
+import br.unb.unbomber.core.Entity;
+
+
+/**
+ * {@link http
+ * ://www.gamedev.net/page/resources/_/technical/game-programming/case
+ * -study-bomberman-mechanics-in-an-entity-component-system-r3159}
+ * 
+ * @author gabrielsr@gmail.com
+ *
+ */
+public class Explosion extends Entity{
+
+	boolean isPassThrough; // Does it pass through soft blocks?
+	boolean isHardPassThrough; // Does it pass through hard blocks?
+	int range;				 // How much further will it propagate?
+	Direction propagationDirection;
+	float countdown; 			// How long does it last?
+	float propagationCountdown; // How long does it take to propagate to the
+								// next square?
+
+
+}
