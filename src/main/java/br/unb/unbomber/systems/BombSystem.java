651851d49d833da7a505cf79d09261f629295cfe
jeffvfa <jeffvfa@hotmail.com>
2014-10-10 16:59:42 -0300

    JavaDOC
    documentação do código

--- src/main/java/br/unb/unbomber/systems/BombSystem.java
+++ src/main/java/br/unb/unbomber/systems/BombSystem.java
@@ -20,81 +20,117 @@ import br.unb.unbomber.event.InAnExplosionEvent;
 import br.unb.unbomber.event.TimeOverEvent;
 
 public class BombSystem extends BaseSystem {
-
+	/* 
+	documented by @author JeffVFA
+	//Define default Value of a bomb  	*/ 
 	private final String TRIGGERED_BOMB_ACTION = "BOMB_TRIGGERED";
-	
+
+	//Define a set of processed events  
 	Set<Event> processedEvents = new HashSet<Event>(500);   
 	
+	/* 
+	bomb constructor 
+	*/
 	public BombSystem() {
 		super();
 	}
 	
+	/* 
+	bomb constructor 
+	@param model 	one instance of the EntityManager
+	*/
 	public BombSystem(EntityManager model) {
 		super(model);
 	}
 	
-	@Override
+	@Override 
+	
+	//Method for the updating of all bombs
 	public void update() {
-		
+		//one instance of the EntityManage
 		EntityManager entityManager = getEntityManager();
 		
-		//Get ActionCommandEvent events
+		//Get ActionCommandEvent events from the entityManager
 		List<Event> actionEvents = entityManager.getEvents(ActionCommandEvent.class);
+		//if exist any event :.
 		if (actionEvents != null) {
+			//for each event do:.
 			for(Event event:actionEvents){
 				ActionCommandEvent actionCommand = (ActionCommandEvent) event;
 				//verify if is it a DROP_BOMB command
 				if ((actionCommand.getType()== ActionType.DROP_BOMB) && (!processedEvents.contains(actionCommand))){
 					BombDropper dropper = (BombDropper) entityManager.getComponent(BombDropper.class, 
 							actionCommand.getEntityId());
-					verifyAndDropBomb(dropper);
+					//if is it drop the bomb 
+					verifyAndDropBomb(dropper); 
+					// and add the event bomb drop to processedEvents set 
 					processedEvents.add(actionCommand);
 				}
 			}
 		}
 		
 		//Triggers active bombs whose timer expired
-		List<Event> timerOvers = entityManager.getEvents(TimeOverEvent.class);
-		if (timerOvers != null) {
+		List<Event> timerOvers = entityManager.getEvents(TimeOverEvent.class); 
+		//Get TimeOverEvent events from the entityManager
+		//if exist any event :.
+		if (timerOvers != null) { 
+			//for each event do:.
 			for (Event event: timerOvers) {
-				TimeOverEvent timeOver = (TimeOverEvent) event;
+				TimeOverEvent timeOver = (TimeOverEvent) event; 
+				//verify if is it a TRIGGERED_BOMB_ACTION command
 				if ((timeOver.getAction().equals(TRIGGERED_BOMB_ACTION)) && (!processedEvents.contains(timeOver))) {
-					createExplosionEvent(timeOver.getOwnerId());
+					//if is it the explosion event starts
+					createExplosionEvent(timeOver.getOwnerId()); 
+					// and add the event time over to processedEvents set 
 					processedEvents.add(timeOver);
 				}
 			}
 		}
 		
 		// InAnExplosionEvent de bombas que estao no range de outras bombas
-		// Bombas devem ser disparadas por efeito cascata
+		// Bombas devem ser disparadas por efeito cascata 
+		//Get InAnExplosionEvent events from the entityManager
 		List<Event> inExplosionEvents = entityManager.getEvents(InAnExplosionEvent.class);
+		//if exist any event :.
 		if (inExplosionEvents != null) {
+			//for each event do:.
 			for(Event event:inExplosionEvents){
 				InAnExplosionEvent inAnExplosion = (InAnExplosionEvent) event;
-				int entityInExplosionId = inAnExplosion.getIdHit();
+				// get the Id of the InAnExplosionEvent event
+				int entityInExplosionId = inAnExplosion.getIdHit(); 
+				//create a new explosive component and set it for any explosive component from the entityManager
 				Explosive bombExplosive = (Explosive) entityManager.getComponent(Explosive.class, entityInExplosionId);
 				Timer bombTimer = (Timer) entityManager.getComponent(Timer.class, entityInExplosionId);
+				//if the explosive exists and its time isn't over and it is active 
 				if ((bombExplosive != null) && (!bombTimer.isOver() && bombTimer.isActive())) {
+					//call the method createExplosionEvent to start a new explosion
 					createExplosionEvent(entityInExplosionId);
-				}
+				} 
+				// and add the event in an explosion to processedEvents set 
 				processedEvents.add(inAnExplosion);
 			}	
 		}
 	}
 	
+	/*  
+	creates a createExplosionEvent event
+	@param bombID	id of the event
+	*/
 	private void createExplosionEvent(int bombID) {
 
-		EntityManager entityManager = getEntityManager();
-		CellPlacement bombPlacement = (CellPlacement) entityManager.getComponent(CellPlacement.class, bombID);
-		Explosive bombExplosive = (Explosive) entityManager.getComponent(Explosive.class, bombID);
-		Timer bombTimer = (Timer) entityManager.getComponent(Timer.class, bombID);
-		ExplosionStartedEvent explosion = new ExplosionStartedEvent();
-		explosion.setEventId(entityManager.getUniqueId());
-		explosion.setOwnerId(bombID);
-		explosion.setInitialPosition(bombPlacement);
-		explosion.setExplosionRange(bombExplosive.getExplosionRange());
-		bombTimer.setActive(false);
-		entityManager.addEvent(explosion);
+		EntityManager entityManager = getEntityManager(); // get the entityManager
+		CellPlacement bombPlacement = (CellPlacement)
+			entityManager.getComponent(CellPlacement.class, bombID); //get the bomb placement from the entityManager
+		Explosive bombExplosive = (Explosive) entityManager.
+			getComponent(Explosive.class, bombID); //get the bomb explosive from the entityManager
+		Timer bombTimer = (Timer) entityManager.getComponent(Timer.class, bombID); // get the bomb timer from the entityManager 
+		ExplosionStartedEvent explosion = new ExplosionStartedEvent(); //starts a new explosion
+		explosion.setEventId(entityManager.getUniqueId()); //set the event ID on the entityManager
+		explosion.setOwnerId(bombID); //set the ownerID to the bobID value
+		explosion.setInitialPosition(bombPlacement); //set the initial position to the position got from the entityManager
+		explosion.setExplosionRange(bombExplosive.getExplosionRange());//set bomb range to the range got from the explosive
+		bombTimer.setActive(false); //desactivate the bomb - when it's active it explode!-
+		entityManager.addEvent(explosion); //add this event to the entityManager
 	}
 	
 	/**
