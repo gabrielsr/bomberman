bc1fdba018acaf62ad57050662d35fd6ed4e97e9
grodrigues <Gabriel Rodrigues>
2014-10-14 22:16:23 -0300

    [PlayerControl Module - Grupo 7]

--- src/main/java/br/unb/unbomber/event/MovementCommandEvent.java
+++ src/main/java/br/unb/unbomber/event/MovementCommandEvent.java
@@ -1,22 +1,49 @@
+//Classe: MovementCommandEvent
+//Versão: 1.00
+//Feito pelo Grupo 7
+
 package br.unb.unbomber.event;
+
 import br.unb.unbomber.core.Event;
+
 public class MovementCommandEvent extends Event {
+
+	//Definição dos tipos de ação capturados
 	public enum MovementType{
 		MOVE_UP,
 		MOVE_RIGHT,
 		MOVE_DOWN,
 		MOVE_LEFT;
 	}
+
 	private final MovementType type;
+	
 	private final int entityId;
+	
+	/**
+	 * Method MovementCommandEvent recebe quem se moveu e armazena no evento.
+	 * @param type tipo do movimento (UP, LEFT, DOWN, RIGHT)
+	 * @param entityId qual entidade se moveu
+	 */
 	public MovementCommandEvent(MovementType type, int entityId){
 		this.type = type;
 		this.entityId = entityId;
 	}
+
+	/**
+	 * Method MovementType resgato o tipo do movimento no evento.
+	 * @return MovementType (UP, LEFT, DOWN, RIGHT)
+	 */
 	public MovementType getType() {
 		return type;
 	}
+	
+	/**
+	 * Method getEntityId resgata o ID da entidade que se moveu no evento
+	 * @return int (entityId)
+	 */
 	public int getEntityId(){
 		return this.entityId;
 	}
+	
 }
--- src/main/java/br/unb/unbomber/event/MovementExecutionEvent.java
+++ src/main/java/br/unb/unbomber/event/MovementExecutionEvent.java
@@ -1,25 +1,59 @@
-package br.unb.unbomber.event; 
-import br.unb.unbomber.component.CellPlacement; 
-import br.unb.unbomber.core.Event; 
+//Classe: MovementExecutionEvent
+//Versão: 1.00
+//Feito pelo Grupo 7
 
-public class MovementExecutionEvent extends Event { 
-	private final CellPlacement initialCell; 
+package br.unb.unbomber.event;
+
+import br.unb.unbomber.component.CellPlacement;
+import br.unb.unbomber.core.Event;
+
+public class MovementExecutionEvent extends Event {
+	
+	private final CellPlacement initialCell;
+	
 	private final CellPlacement finalCell;
+	
 	private final int entityId;
 
-	public MovementExecutionEvent(CellPlacement initialCell, CellPlacement finalCell, int entityId) { 
-		super(); this.initialCell = initialCell; this.finalCell = finalCell; this.entityId = entityId; 
+	/**
+	 * Method MovementExecutionEvent constroi o evento com a posição inicial
+	 * e final, junto com o id da entidade em movimento.
+	 * @param initialCell posição inicial da entidade
+	 * @param finalCell posição final da entidade
+	 * @param entityId id da entidade
+	 */
+	public MovementExecutionEvent(CellPlacement initialCell,
+			CellPlacement finalCell, int entityId) {
+		super();
+		this.initialCell = initialCell;
+		this.finalCell = finalCell;
+		this.entityId = entityId;
 	}
 
-	public CellPlacement getInitialCell() { 
-		return initialCell; 
+	/**
+	 * Method getInitialCell resgata a posição inicial da entidade
+	 * no evento.
+	 * @return initialCell
+	 */
+	public CellPlacement getInitialCell() {
+		return initialCell;
 	}
 
-	public CellPlacement getFinalCell() { 
-		return finalCell; 
+	/**
+	 * Method getFinalCell resgata a posição final da entidade
+	 * no evento.
+	 * @return finalCell
+	 */
+	public CellPlacement getFinalCell() {
+		return finalCell;
 	}
 
-	public int getEntityId() { 
-		return entityId; 
-	} 
+	/**
+	 * Method getEntityId resgata o ID da entidade que se moveu no evento
+	 * @return int (entityId)
+	 */
+	public int getEntityId() {
+		return entityId;
+	}
+	
 }
