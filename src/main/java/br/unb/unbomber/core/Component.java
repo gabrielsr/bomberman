b9f096f255ec06b8d45258ab29eced558fd01b64
grodrigues <Gabriel Rodrigues>
2014-10-16 08:22:45 -0300

    fixing a proteced method issue.

--- src/main/java/br/unb/unbomber/core/Component.java
+++ src/main/java/br/unb/unbomber/core/Component.java
@@ -24,12 +24,13 @@ public class Component {
 	 * 
 	 * The Id is setted when you add the component to a entity. 
 	 * 
-	 * TODO change visibility to protect
+	 * TODO remove calls to setEntityID outside this package
+	 *  and change visibility to protect
 	 * 
 	 * @param entityId the new entity id
 	 * @see Entity#addComponent(Component)
 	 */
-	public final void setEntityId(int entityId) {
+	public void setEntityId(int entityId) {
 		this.entityId = entityId;
 	}
 
