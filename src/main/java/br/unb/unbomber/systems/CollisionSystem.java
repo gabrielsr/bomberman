79fe4eccaa1d5486b75adeb5f44496f0971abc52
Maxwell Fernandes <low-id@gleba>
2014-10-21 10:15:38 -0200

    Modificação no teste e correção ortográfica nos comentários

--- src/main/java/br/unb/unbomber/systems/CollisionSystem.java
+++ src/main/java/br/unb/unbomber/systems/CollisionSystem.java
@@ -1,166 +1,233 @@
 /********************************************************************************************************************************
-*Grupo 2:
-*Maxwell Moura Fernandes     - 10/0116175
-*João Paulo Araujo           -
-*Alexandre Magno             -
-*Marcelo Giordano            -
-*********************************************************************************************************************************/
+ *Grupo 2:
+ *Maxwell Moura Fernandes     - 10/0116175
+ *João Paulo Araujo           -
+ *Alexandre Magno             -
+ *Marcelo Giordano            -
+ *********************************************************************************************************************************/
 package br.unb.unbomber.systems;
-
 
 import java.util.List;
 
 import br.unb.unbomber.component.CellPlacement;
 import br.unb.unbomber.core.BaseSystem;
 import br.unb.unbomber.core.Component;
+import br.unb.unbomber.core.EntityManager;
 import br.unb.unbomber.core.Event;
 import br.unb.unbomber.event.CollisionEvent;
 import br.unb.unbomber.event.MovedEntityEvent;
 
 public class CollisionSystem extends BaseSystem {
 
-	//@ override
-	public void update(){
+	// Não sei a utilidade desses construtores.
+	public CollisionSystem() {
+		super();
+	}
+
+	public CollisionSystem(EntityManager entityManager) {
+		super(entityManager);
+	}
+
+	// @ override
+	public void update() {
 		testOfCollision();
 
 	}
 
-	//função que verifica se houve colisão. Para cada colisão encontrada, um evento (CollisionEvent) com o
-	//id da entidade fonte e o id da entidade alvo é adicionado ao conjuto de eventos.
-	public void testOfCollision(){
-		//Get a CellPlacement components
-		List<Component> placeComponents = getEntityManager().getComponents(CellPlacement.class);
-		//Get a CollisionEvent events
-		List<Event> movedEntityEvents = getEntityManager().getEvents(MovedEntityEvent.class);
-		//Cria um array que conterá as colisões já feitas para não gerar dois eventos para a mesma colisão.
-		//É um array que em cada posição terá um array de inteiros contendo o id source e target da colisão.
-		int collMade[][] = new int[ movedEntityEvents.size() ][ 2 ];
-		//Receberá a dupla de id's das entidades que colidiram.
-		//int coll[] = new int[2];
+	// função que verifica se houve colisão. Para cada colisão encontrada, um
+	// evento (CollisionEvent) com o
+	// id da entidade fonte e o id da entidade alvo é adicionado ao conjuto de
+	// eventos.
+	public void testOfCollision() {
+		// Get a CellPlacement components
+		List<Component> placeComponents = getEntityManager().getComponents(
+				CellPlacement.class);
+		// Get a CollisionEvent events
+		List<Event> movedEntityEvents = getEntityManager().getEvents(
+				MovedEntityEvent.class);
+		// Cria um array que conterá as colisões já feitas para não gerar dois
+		// eventos para a mesma colisão.
+		// É um array que em cada posição terá um array de inteiros contendo o
+		// id source e target da colisão.
+		int collMade[][] = new int[movedEntityEvents.size()][2];
+		// Receberá a dupla de id's das entidades que colidiram.
+		// int coll[] = new int[2];
 		int aux;
-		//Declared variables used inside the for 
+		// Declared variables used inside the for
 		MovedEntityEvent movedEntityEvent;
-		
-		//itera pela lista de entidades que se moveram. O movimento é indicado pela existência de um evento
-		//do tipo MovedEntityEvents.
-		for(Event event:movedEntityEvents){
+
+		// itera pela lista de entidades que se moveram. O movimento é indicado
+		// pela existência de um evento
+		// do tipo MovedEntityEvents.
+		for (Event event : movedEntityEvents) {
 			CellPlacement cellPlacement1;
 
-			//observe que movedEntityEvent não é uma lista.
+			// observe que movedEntityEvent não é uma lista.
 			movedEntityEvent = (MovedEntityEvent) event;
-			//obtém o CellPlacement de uma entidade que se moveu. Onde o id é obtido a partir de
-			//movedEntityEvent.
-			//cellPlacement1 não é necessário de verdade já que é possível obter (x,y) de MovedEntityEvent.
-			cellPlacement1 = (CellPlacement) getEntityManager().getComponent(CellPlacement.class,
-					movedEntityEvent.getEntityId());
-			
-			//TODO testar se a colisão se dá entre softblock (bomba, powerup) e personagem.
-			
-			//compara as posições de uma das entidades que se moveram com todos os elementos da lista
-			//de CellPlacement, i.e, todas as entidades do jogo.
-			for(Component component:placeComponents){
-				//uma componente específica da lista de componentes placements. Observe que placeComponents 
-				//contém todas as entidades (hardblock, softblock, personagem, etc).
+			// obtém o CellPlacement de uma entidade que se moveu. Onde o id é
+			// obtido a partir de
+			// movedEntityEvent.
+			// cellPlacement1 não é necessário de verdade já que é possível
+			// obter (x,y) de MovedEntityEvent.
+			cellPlacement1 = (CellPlacement) getEntityManager().getComponent(
+					CellPlacement.class, movedEntityEvent.getEntityId());
+
+			// TODO testar se a colisão se dá entre softblock (bomba, powerup) e
+			// personagem.
+
+			// compara as posições de uma das entidades que se moveram com todos
+			// os elementos da lista
+			// de CellPlacement, i.e, todas as entidades do jogo.
+			for (Component component : placeComponents) {
+				// uma componente específica da lista de componentes placements.
+				// Observe que placeComponents
+				// contém todas as entidades (hardblock, softblock, personagem,
+				// etc).
 				CellPlacement cellPlacement2;
 				cellPlacement2 = (CellPlacement) component;
 
-				//Se os ids forem diferentes verifica se houve colisão.
-				if(movedEntityEvent.getEntityId() != cellPlacement2.getEntityId()){
-					//se o if for verdadeiro significa que (x1,y1) = (x2,y2), i.e, houve colisão entre alguém que se
-					//moveu e alguma outra entidade, podendo esta ter se movido ou não.
-					//Se ninguém se moveu não pode ter havido nenhuma colisão.
-					if( (cellPlacement1.getCellX() == cellPlacement2.getCellX()) && 
-							(cellPlacement1.getCellY() == cellPlacement2.getCellY()) ){
-						
-						//Testa se a colisão do if acima já ocorreu.
-						//Caso a colisão em questão já tenha ocorrido itera mais uma vez.
-						boolean test = false;
-						test = collisionMade(collMade, movedEntityEvent.getEntityId(),
-											cellPlacement2.getEntityId() );
-						if( test == true )
-							continue;
-						
-						//Passa o id de uma entidade e 
-						//recebe um evento relacionado ao movimento de uma entidade.
-						//Essa entidade pode ou não ter se movido. Caso não tenha o resultado será NULL.
-						//MovedEntityEvent tempMEntityE = getEntityManager().getEvent(MovedEntityEvent.class, cellPlacement2.getEntityId() );
-						MovedEntityEvent tempMEntityE = getEvent( movedEntityEvents ,cellPlacement2.getEntityId() );
+				// Se os ids forem diferentes verifica se houve colisão.
+				if (movedEntityEvent.getEntityId() != cellPlacement2
+						.getEntityId()) {
+					// se o if for verdadeiro significa que (x1,y1) = (x2,y2),
+					// i.e, houve colisão entre alguém que se
+					// moveu e alguma outra entidade, podendo esta ter se movido
+					// ou não.
+					// Se ninguém se moveu não pode ter havido nenhuma colisão.
+					if ((cellPlacement1.getCellX() == cellPlacement2.getCellX())
+							&& (cellPlacement1.getCellY() == cellPlacement2
+									.getCellY())) {
 
-						//TODO verificar para outras direções.
-						/*Se duas entidades estão se movendo na mesma direção, por exemplo > >, a colisão(quando ocorre)
-						 * se dá da mais rápida na mais lenta. Agora quando a colisão ocorre e as entidades têm sentidos 
-						 * diferente(> <, > ^), a colisão é dupla, independentemente da velocidade. Portanto, para este último caso,
-						 * dois eventos são criados, uma para cada choque. Ex.: eventoX, A colide com B e eventoY, B colide com A.*/
-						//O problema dos duplos eventos de colisão foi corrigido. Desprese essa parte do comentário acima.
-						if( tempMEntityE != null && ( movedEntityEvent.getDirection() == tempMEntityE.getDirection() ) )
-							if( movedEntityEvent.getSpeed() > tempMEntityE.getSpeed() ){
-								//o primeiro argumento é quem colide e o segundo em que se colide.
-								makeCollisionEvent( movedEntityEvent.getEntityId(), tempMEntityE.getEntityId() );
-								//adiciona a colisão ao array de controle de colisões.
+						// Testa se a colisão do if acima já ocorreu.
+						// Caso a colisão em questão já tenha ocorrido itera
+						// mais uma vez.
+						boolean test = false;
+						test = collisionMade(collMade,
+								movedEntityEvent.getEntityId(),
+								cellPlacement2.getEntityId());
+						if (test == true)
+							continue;
+
+						// Passa o id de uma entidade e
+						// recebe um evento relacionado ao movimento de uma
+						// entidade.
+						// Essa entidade pode ou não ter se movido. Caso não
+						// tenha o resultado será NULL.
+						// MovedEntityEvent tempMEntityE =
+						// getEntityManager().getEvent(MovedEntityEvent.class,
+						// cellPlacement2.getEntityId() );
+						MovedEntityEvent tempMEntityE = getEvent(
+								movedEntityEvents, cellPlacement2.getEntityId());
+
+						// TODO verificar para outras direções.
+						/*
+						 * Se duas entidades estão se movendo na mesma direção,
+						 * por exemplo > >, a colisão(quando ocorre) se dá da
+						 * mais rápida na mais lenta. Agora quando a colisão
+						 * ocorre e as entidades têm sentidos diferente(> <, >
+						 * ^), a colisão é dupla, independentemente da
+						 * velocidade. Portanto, para este último caso, dois
+						 * eventos são criados, uma para cada choque. Ex.:
+						 * eventoX, A colide com B e eventoY, B colide com A.
+						 */
+						// O problema dos duplos eventos de colisão foi
+						// corrigido. Despreze essa parte do comentário acima.
+						if (tempMEntityE != null
+								&& (movedEntityEvent.getDirection() == tempMEntityE
+										.getDirection()))
+							if (movedEntityEvent.getSpeed() > tempMEntityE
+									.getSpeed()) {
+								// o primeiro argumento é quem colide e o
+								// segundo em que se colide.
+								makeCollisionEvent(
+										movedEntityEvent.getEntityId(),
+										tempMEntityE.getEntityId());
+								// adiciona a colisão ao array de controle de
+								// colisões.
 								aux = collMade.length;
-								collMade[ aux ][0] = movedEntityEvent.getEntityId();
-								collMade[ aux ][1] = tempMEntityE.getEntityId();
-							}
-							else{
-								//Se as velocidades forem iguais o casamento de coordenadas é impossível.
-								//Portando neste laço a entidade indicada por movedEntityEvent tem velocidade menor 
-								//que a outra entidade.
-								makeCollisionEvent(tempMEntityE.getEntityId(),movedEntityEvent.getEntityId());
+								collMade[aux][0] = movedEntityEvent
+										.getEntityId();
+								collMade[aux][1] = tempMEntityE.getEntityId();
+							} else {
+								// Se as velocidades forem iguais o casamento de
+								// coordenadas é impossível.
+								// Portando neste laço a entidade indicada por
+								// movedEntityEvent tem velocidade menor
+								// que a outra entidade.
+								makeCollisionEvent(tempMEntityE.getEntityId(),
+										movedEntityEvent.getEntityId());
 								aux = collMade.length;
-								collMade[ aux ][0] = tempMEntityE.getEntityId();
-								collMade[ aux ][1] = movedEntityEvent.getEntityId();
+								collMade[aux][0] = tempMEntityE.getEntityId();
+								collMade[aux][1] = movedEntityEvent
+										.getEntityId();
 							}
-						else{
-							//Um de duas coisa aconteceram:
-							//1)Houve uma colisão entre uma entidade que se moveu e outra que ficou parada.
-							//2)As duas entidades se moveram e suas direções são diferentes.
-							makeCollisionEvent(movedEntityEvent.getEntityId(), cellPlacement2.getEntityId());
+						else {
+							// Um de duas coisa aconteceram:
+							// 1)Houve uma colisão entre uma entidade que se
+							// moveu e outra que ficou parada.
+							// 2)As duas entidades se moveram e suas direções
+							// são diferentes.
+							makeCollisionEvent(movedEntityEvent.getEntityId(),
+									cellPlacement2.getEntityId());
 							aux = collMade.length;
-							collMade[ aux ][0] = movedEntityEvent.getEntityId();
-							collMade[ aux ][1] = cellPlacement2.getEntityId();
+							collMade[aux][0] = movedEntityEvent.getEntityId();
+							collMade[aux][1] = cellPlacement2.getEntityId();
 						}
 
-						//passa para a função que cria o collisionEvent o id da entidade que é a fonte da colisão
-						//e o id da entidade que é o alvo da colisão.
-						//Se essa chamada ocorreu então se deu entre alguma entidade que se moveu e outra que ficou parada.
-						makeCollisionEvent( movedEntityEvent.getEntityId(), cellPlacement2.getEntityId() );
+						/*
+						 * Ignore esse comentário. A situação a que ele se
+						 * refere foi tratada no else acima.
+						 * 
+						 * Passa para a função que cria o collisionEvent o id d
+						 * entidade que é a fonte da colisão e o id da entidade
+						 * que é o alvo da colisão. Se essa chamada ocorreu
+						 * então se deu entre alguma entidade que se moveu e
+						 * outra que ficou parada.
+						 * makeCollisionEvent(movedEntityEvent
+						 * .getEntityId(),cellPlacement2.getEntityId());
+						 */
 
-					}//fim do if onde se testa o casamento de coordenadas.
-				}//fim do if onde se testa a diferença de id's.
-			}//fim do segundo for. Compara quem se moveu(entidade) com todas as entidades do "grid".
+					}// fim do if onde se testa o casamento de coordenadas.
+				}// fim do if onde se testa a diferença de id's.
+			}// fim do segundo for. Compara quem se moveu(entidade) com todas as
+				// entidades do "grid".
 
+		}// fim do primeiro for. Percorrimento de todos os eventos relacinados
+			// ao movimento de entidades.
+	}// fim do testOfCollision
 
-		}//fim do primeiro for. Percorrimento de todos os eventos relacinados ao movimento de entidades.
-	}//fim do testOfCollision
-
-	private void makeCollisionEvent(int sourceId, int  targetId){
-		//prepara o objeto para ser adicionado ao conjunto de eventos de colisão.
+	private void makeCollisionEvent(int sourceId, int targetId) {
+		// prepara o objeto para ser adicionado ao conjunto de eventos de
+		// colisão.
 		CollisionEvent collisionEvent = new CollisionEvent(sourceId, targetId);
 
-		//adiciona o novo evento de colisão
+		// adiciona o novo evento de colisão
 		getEntityManager().addEvent(collisionEvent);
 	}
-	
-	//Função provisória. O ideal era que o EntityManager tivesse um método que retornasse um evento específico
-	//como ele tem para componente.
-	private MovedEntityEvent getEvent( List<Event> movedEntityList, int id ){
+
+	// Função provisória. O ideal era que o EntityManager tivesse um método que
+	// retornasse um evento específico
+	// como ele tem para componente.
+	private MovedEntityEvent getEvent(List<Event> movedEntityList, int id) {
 		MovedEntityEvent mee;
-		for(Event evt : movedEntityList){
+		for (Event evt : movedEntityList) {
 			mee = (MovedEntityEvent) evt;
-			if( mee.getEntityId() == id )
+			if (mee.getEntityId() == id)
 				return mee;
 		}
 		return null;
 	}
-	
-	//Verifica se já aconteceu alguma colisão entre id1 e id2. Se sim retorna true. 
-	private boolean collisionMade( int[][] collMade, int id1, int id2){
-		
-		for(int[] aux : collMade){
-			if( (aux[0] == id1 && aux[1] == id2) || (aux[1] == id1 && aux[0] == id2) )
+
+	// Verifica se já aconteceu alguma colisão entre id1 e id2. Se sim retorna
+	// true.
+	private boolean collisionMade(int[][] collMade, int id1, int id2) {
+
+		for (int[] aux : collMade) {
+			if ((aux[0] == id1 && aux[1] == id2)
+					|| (aux[1] == id1 && aux[0] == id2))
 				return true;
 		}
 		return false;
 	}
 
-}//fim da classe CollisionSystem
+}// fim da classe CollisionSystem
---
+++ src/test/java/br/unb/unbomber/systems/CollisionSystemTestCase.java
@@ -0,0 +1,189 @@
+/********************************************************************************************************************************
+ *Grupo 2:
+ *Maxwell Moura Fernandes     - 10/0116175
+ *Jo�o Paulo Araujo           -
+ *Alexandre Magno             -
+ *Marcelo Giordano            -
+ *********************************************************************************************************************************/
+package br.unb.unbomber.systems;
+
+import static junit.framework.Assert.*;
+import br.unb.unbomber.core.Event;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.unb.unbomber.component.CellPlacement;
+import br.unb.unbomber.component.Direction;
+import br.unb.unbomber.component.Movable;
+import br.unb.unbomber.core.Entity;
+import br.unb.unbomber.core.EntityManager;
+import br.unb.unbomber.core.EntityManagerImpl;
+import br.unb.unbomber.event.CollisionEvent;
+import br.unb.unbomber.event.MovedEntityEvent;
+import br.unb.unbomber.systems.CollisionSystem;
+
+public class CollisionSystemTestCase {
+
+	EntityManager entityManager;
+	CollisionSystem collisionSystem;
+
+	// O Before indica que esse método será executado antes.
+	@Before
+	public void setUp() throws Exception {
+
+		// init a new system for each test case
+
+		// Cria um objeto de EntityManager.
+		EntityManagerImpl.init();
+		// Obtém a referência do objeto criado a cima.
+		entityManager = EntityManagerImpl.getInstance();
+		// Cria o objeto que será testado.
+		collisionSystem = new CollisionSystem(entityManager);
+	}
+
+	/*
+	 * Por enquanto só tem dois testes. Um para testar se um único evento de
+	 * colisão é gerando para uma uma colisão. Outro para determinar se a origem
+	 * da colisão é a entidade mais rápida, quando as entidades tem a mesma
+	 * direção.
+	 */
+
+	@Test
+	public void SingleCollision() {
+
+		// Cria uma entidade que pode se mover e outra que não pode.
+		Entity entityA = createEntity(0, 0, 2);
+		Entity entityB = createEntity(0, 0);
+
+		// Cria um evento indicando que a entidade A se moveu no tick anterior.
+		// Eu deixei a atribuição, mas ela não é necessária.
+		MovedEntityEvent mEntityA = createMovedEntityEvent(
+				entityA.getEntityId(), Direction.RIGHT);
+		// MovedEntityEvent mEntityB =
+		// createMovedEntityEvent(entityB.getEntityId(), Direction.RIGHT);
+
+		// Atualliza o collision system e verifica se uma lista com a quantidade
+		// correta de eventos
+		// foi gerada.
+		// Espera-se que um único evento de colisão tenha sido gerado.
+		collisionSystem.update();
+		List<Event> collisionEvents = entityManager
+				.getEvents(CollisionEvent.class);
+
+		assertEquals(1, collisionEvents.size());
+	}
+
+	@Test
+	public void CollisionAB() {
+		// Cria duas entidades que podem se mover, A e B.
+		Entity entityA = createEntity(0, 0, 2);
+		Entity entityB = createEntity(0, 0, 0);
+
+		// Cria dois eventos indicando que as entidades A e B se moveram no tick
+		// anterior.
+		// Deixei as atribuições mas elas não são necessárias.
+		MovedEntityEvent mEntityA = createMovedEntityEvent(
+				entityA.getEntityId(), Direction.RIGHT);
+		MovedEntityEvent mEntityB = createMovedEntityEvent(
+				entityB.getEntityId(), Direction.RIGHT);
+
+		// Atualiza o collision system e verifica se para a colisão gerada a
+		// fonte é a entidade A.
+		collisionSystem.update();
+		List<Event> collisionEvents = entityManager
+				.getEvents(CollisionEvent.class);
+
+		for (Event event : collisionEvents) {
+			CollisionEvent collisionEvent = (CollisionEvent) event;
+			assertEquals(entityA.getEntityId(), collisionEvent.getSourceId());
+		}
+	}
+
+	/* Métodos usados pelos casos de teste acima */
+
+	// Existe uma sobrecarga do método createEntity.
+	// Passando apenas dois argumentos como a versão abaixo, cria-se uma
+	// entidade que não pode se mover (softblock ou powerup).
+
+	// Cria entidades imóveis.
+	private Entity createEntity(int x, int y) {
+
+		// Cria uma entidade, identificada por um id único.
+		Entity anEntity = entityManager.createEntity();
+
+		// Cria um component Movable.
+		// Seta a velocidade em 2 unidades de velocidade.
+		// Movable movable = new Movable();
+		// movable.setSpeed(velocidade);
+
+		// Create um componet Placement
+		// Seta a posição
+		CellPlacement placement = new CellPlacement();
+		placement.setCellX(x);
+		placement.setCellY(y);
+
+		// Adidiciona os componentes criados a entidade.
+		// anEntity.addComponent(movable);
+		anEntity.addComponent(placement);
+
+		// Atualiza o entityManager adicionando as novas componentes ao conjunto
+		// de componetes
+		// do jogo.
+		// Lembre que cada componente tem um id que indica a qual entidade ela
+		// pertence.
+		entityManager.update(anEntity);
+
+		return anEntity;
+	}// Fim do criador de entidades móveis
+
+	// Cria entidades móveis.
+	private Entity createEntity(int x, int y, int velocidade) {
+
+		// Cria uma entidade, identificada por um id único.
+		Entity anEntity = entityManager.createEntity();
+
+		// Cria um component Movable.
+		// Seta a velocidade em 2 unidades de velocidade.
+		Movable movable = new Movable();
+		movable.setSpeed(velocidade);
+
+		// Create um componet Placement
+		// Seta a posição
+		CellPlacement placement = new CellPlacement();
+		placement.setCellX(x);
+		placement.setCellY(y);
+
+		// Adidiciona os componentes criados a entidade.
+		anEntity.addComponent(movable);
+		anEntity.addComponent(placement);
+
+		// Atualiza o entityManager adicionando as novas componentes ao conjunto
+		// de componetes
+		// do jogo.
+		// Lembre que cada componente tem um id que indica a qual entidade ela
+		// pertence.
+		entityManager.update(anEntity);
+
+		return anEntity;
+	}// Fim do criado de entidades móveis.
+
+	// Cria uma envento indicando que determinada entidade se moveu no tick
+	// anterior.
+	private MovedEntityEvent createMovedEntityEvent(int id, Direction direction) {
+		// Cria um movedEntityEvent.
+		// Caracterisa o evento com informações da entidade associada, i.e, seu
+		// id etc.
+		MovedEntityEvent event = new MovedEntityEvent();
+		event.setEventId(id);
+		event.setDirection(direction);
+
+		// Adiciona o evento criado a lista de eventos de todo o jogo.
+		entityManager.addEvent(event);
+
+		return event;
+
+	}
+}
