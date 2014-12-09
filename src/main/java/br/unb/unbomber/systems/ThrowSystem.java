822fee0718e783e4fd8220d0edac22237d5ded5c
Gabriel Rodrigues <gabrielsr@gmail.com>
2014-12-08 23:14:43 -0200

    Comentando verificao se possui Blue Glove para poder compilar e organizando imports.

--- src/main/java/br/unb/unbomber/systems/ThrowSystem.java
+++ src/main/java/br/unb/unbomber/systems/ThrowSystem.java
@@ -13,22 +13,18 @@ package br.unb.unbomber.systems;
 
 import java.util.List;
 
-import org.apache.tools.ant.filters.LineContains.Contains;
-
 import br.unb.unbomber.component.CellPlacement;
 import br.unb.unbomber.component.Explosion;
+import br.unb.unbomber.component.PowerUp;
 import br.unb.unbomber.core.BaseSystem;
 import br.unb.unbomber.core.Component;
-import br.unb.unbomber.core.Entity;
 import br.unb.unbomber.core.EntityManager;
 import br.unb.unbomber.core.Event;
 import br.unb.unbomber.event.ActionCommandEvent;
 import br.unb.unbomber.event.ActionCommandEvent.ActionType;
-import br.unb.unbomber.event.BombThrowEvent;
 import br.unb.unbomber.event.CollisionEvent;
 import br.unb.unbomber.event.MovementCommandEvent;
 import br.unb.unbomber.event.MovementCommandEvent.MovementType;
-import br.unb.unbomber.event.MovementMadeEvent;
 
 public class ThrowSystem extends BaseSystem {
 
@@ -80,7 +76,7 @@ public class ThrowSystem extends BaseSystem {
 			 * < condicao que verifica se a blue glove esta ativo na entidade
 			 * verificada
 			 */
-			if (powerup.getBlueGlove() == true) { // fizemos isso pois ainda nao
+			if (/* powerup.getBlueGlove() == */ true) { // TODO fizemos isso pois ainda nao
 													// foi implementado o power
 													// up
 				/**
